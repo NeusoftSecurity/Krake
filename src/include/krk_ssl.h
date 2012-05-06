6116887dd1b7408b6b24d4f3deaf1a29872dcf95
Paul Yang <paulyang.inf@gmail.com>
2012-05-06 17:44:34 +0800

    [new feature][SSL]: add new ssl support. this is the dummy files

---
+++ src/include/krk_ssl.h
@@ -0,0 +1,17 @@
+/**
+ * krk_ssl.h - Krake ssl
+ * 
+ * Copyright (c) 2011 Yang Yang <paulyang.inf@gmail.com>
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#ifndef __KRK_SSL_H__
+#define __KRK_SSL_H__
+
+#endif
